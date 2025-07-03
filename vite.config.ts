fb3f3db660145b16b402ebb30cc5fe7c6561aba8
javier289 <antoniojofre89@gmail.com>
2025-07-03 18:12:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { tempo } from "tempo-devtools/dist/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   optimizeDeps: {
     entries: ["src/main.tsx", "src/tempobook/**/*"],
   },
